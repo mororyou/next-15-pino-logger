02698c937886b1e7ef1dae7397211d1d3d49a214
R.Moroizumi <m.ryousuke0401@icloud.com>
2024-12-02 19:04:33 +0900

    feat: update next.config.ts to include serverExternalPackages for pino

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // next 15~
+  serverExternalPackages: ['pino'],
+  // next ~14
+  // experimental: {
+  //   serverComponentsExternalPackages: ['pino'],
+  // },
 };
 
 export default nextConfig;
